83b5cc793bd1dfb0bdb71bd1bd75b328f73d0ba3
swastikabehera76-spec <swastikabehera76@gmail.com>
2025-11-24 10:19:50 +0000

    created a basic server

---
+++ server.py
@@ -0,0 +1,11 @@
+    from http.server import SimpleHTTPRequestHandler,HTTPServer
+    class MuHandler(SimpleHTTPRequestHandler):
+        def do_GET(self):
+            if self.path == '/':
+                  self.path = 'index.html'
+                return super().do_GET()
+
+    if __name__=="__main__":
+    server = HTTPServer(('localhost', 8000), MyHandler)
+    print("Serving on http://localhost:8000")
+    server.serve_forever()
